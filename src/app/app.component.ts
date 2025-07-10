afdc5c7dcb82e83033838b0f0905fa8e4057bda7
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-07-10 18:15:06 -0300

    adicionando mat-toolbar ao projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'crud-angular';
