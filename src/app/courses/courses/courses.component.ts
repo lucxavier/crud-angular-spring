fc3a0aabe9853f0e3d5da82503a428986e7eeb4e
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-07-10 19:18:52 -0300

    criando tabela de listagem de cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end' },
+    { _id: '2', name: 'React', category: 'Front-end' },
+    { _id: '3', name: 'Vue', category: 'Front-end' },
+    { _id: '4', name: 'Node', category: 'Back-end' },
+    { _id: '5', name: 'Java', category: 'Back-end' },
+  ];
+  displayedColumns = ['_id', 'name', 'category'];
 }
