b384e61c0811c212ed4941824d78d16f51f37a70
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-07-16 14:59:40 -0300

    testando a aplicação com um Hello World

--- backend/src/main/java/com/lucas/crud_spring/CrudSpringApplication.java
+++ backend/src/main/java/com/lucas/crud_spring/CrudSpringApplication.java
@@ -2,8 +2,10 @@ package com.lucas.crud_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.lucas")
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
