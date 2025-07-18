187bff9eaca936ad2b96a752920b86a9c6e86ab6
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-07-18 00:05:30 -0300

    criando função de create curso no backend

--- backend/src/main/java/com/lucas/controller/CourseController.java
+++ backend/src/main/java/com/lucas/controller/CourseController.java
@@ -2,8 +2,12 @@ package com.lucas.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.model.Course;
@@ -23,4 +27,9 @@ public class CourseController {
     return courseRepository.findAll();
   }
   
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create (@RequestBody Course course){
+    return courseRepository.save(course);
+  }
 }
