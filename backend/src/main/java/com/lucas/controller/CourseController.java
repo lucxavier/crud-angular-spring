575ba2e1a3597fd273098918d8703bbe74d999aa
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-07-20 23:45:23 -0300

    implementando o método de atualização de curso no CourseController

--- backend/src/main/java/com/lucas/controller/CourseController.java
+++ backend/src/main/java/com/lucas/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,10 +35,19 @@ public class CourseController {
     return ResponseEntity.of(courseRepository.findById(id));
   }
 
-
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
   public Course create (@RequestBody Course course){
     return courseRepository.save(course);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    return ResponseEntity.of(courseRepository.findById(id)
+        .map(existingCourse -> {
+          existingCourse.setName(course.getName());
+          existingCourse.setCategory(course.getCategory());
+          return courseRepository.save(existingCourse);
+        }));
   }
 }
