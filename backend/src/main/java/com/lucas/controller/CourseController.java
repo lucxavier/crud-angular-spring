a37c44c7a228b9b63e4d864df56b132345a178ee
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-07-22 09:34:06 -0300

    implementando o método de exclusão de curso no CourseController

--- backend/src/main/java/com/lucas/controller/CourseController.java
+++ backend/src/main/java/com/lucas/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,15 @@ public class CourseController {
           existingCourse.setCategory(course.getCategory());
           return courseRepository.save(existingCourse);
         }));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    return courseRepository.findById(id)
+        .map(_ -> {
+          courseRepository.deleteById(id);
+          return ResponseEntity.noContent().<Void>build();
+        })
+        .orElse(ResponseEntity.notFound().build());
   }
 }
