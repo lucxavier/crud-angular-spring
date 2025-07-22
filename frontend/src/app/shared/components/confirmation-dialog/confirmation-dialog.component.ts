91737d7256cfddecf10724f6ee19fe549ee9dcca
lucas.henrique.xavier <lucas.h.xavier@gmail.com>
2025-07-22 09:34:57 -0300

    implementando o componente de diálogo de confirmação com template e lógica de fechamento

---
+++ frontend/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.scss',
+})
+export class ConfirmationDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {}
+
+  onConfirm(result: boolean): void {
+    this.dialogRef.close(result);
+  }
+}
